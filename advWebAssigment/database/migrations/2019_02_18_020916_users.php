5a6735ab046445595cae7c7ccc77c453450f6a2c
wei siong <swsiong97@gmail.com>
2019-02-18 10:18:06 +0800

    add the users table in migration

---
+++ advWebAssigment/database/migrations/2019_02_18_020916_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increment('id');
+            $table->string('user-_type');
+            $table->string('email');
+            $table->string('name');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
